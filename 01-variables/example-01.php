f89a9bd1b1fdee64d5242fba8fb85d8f6b7ec8e4
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2024-01-08 22:31:56 -0300

    Update example-01.php

--- 01-variables/example-01.php
+++ 01-variables/example-01.php
@@ -5,12 +5,14 @@
 $name = "Learning PHP";
 echo "<h1>" . $name . "</h1>";
 
+// Variables are declared using a dollar sign '$' plus name - $my_variable
 $numerOfVisitors = 237;
 echo "<br><hr>";
 echo "<p>There was around " . $numerOfVisitors . "visitors</p>";
 
 echo "<br>";
 
+// var_dump() shows the type of the variable and its value aswell
 var_dump($numerOfVisitors);
 
 ?>
