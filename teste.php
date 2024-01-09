b858dae38a67413c0c057b72591a5a12a898b94b
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2024-01-08 22:02:13 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,6 +1,13 @@
 <?php
 
 echo "<h1>Testando mecanicas basicas do git</h1>";
-echo "teste de git";
+echo "teste de git<br>";
+
+echo "
+<div style='width:200px; height:200px; border:1px solid black; background-color:gray display:flex; justify-content:center; align-itens:center;'>
+TESTE
+</div>
+";
+
 
 ?>
