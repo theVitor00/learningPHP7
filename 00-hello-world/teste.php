a1573119ba520b2b4824801a9ebd7d01daa5e1d2
Pablo Vitor <thevitor00@gmail.com>
2024-01-08 22:29:36 -0300

    Variables lesson and few code fixes

--- 00-hello-world/teste.php
+++ 00-hello-world/teste.php
@@ -8,7 +8,7 @@ echo "Hello Wold<br>";
 echo "teste de git<br>";
 
 echo "
-<div style='width:200px; height:200px; border:1px solid black; background-color:gray display:flex; justify-content:center; align-itens:center;'>
+<div style='width:200px; height:200px; border:1px solid black; background-color:gray; display:flex; justify-content:center; align-items: center;'>
 TESTE
 </div>
 ";
---
+++ 01-variables/example-01.php
@@ -0,0 +1,16 @@
+<!-- VARIABLES --> 
+
+<?php
+
+$name = "Learning PHP";
+echo "<h1>" . $name . "</h1>";
+
+$numerOfVisitors = 237;
+echo "<br><hr>";
+echo "<p>There was around " . $numerOfVisitors . "visitors</p>";
+
+echo "<br>";
+
+var_dump($numerOfVisitors);
+
+?>
